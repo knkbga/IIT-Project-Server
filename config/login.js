70a4c06db2ccaa014ffd5a92de1dd9018fd02339
Kanak Bagga <kanakbagga1@gmail.com>
2017-06-17 16:11:58 +0530

    app_code check added in top-most priority.

--- config/login.js
+++ config/login.js
@@ -6,38 +6,40 @@ var user = require('../config/models');
 exports.login = function(email,password,app_code,callback) {
 
     console.log("In login.js");
-    user.find({'person_credentials.email': email},function(err,users)
+    if(app_code == "mzf")
     {
-        console.log("Email id found.");
-        if(users.length != 0)
+        user.find({'person_credentials.email': email},function(err,users)
         {
-            var unique_id = users[0].person_credentials.token;
-            var temp = users[0].person_credentials.salt;
-            var hash_db = users[0].person_credentials.hashed_password;
-            var name = users[0].person_credentials.name;
-            var id = users[0]._id;
-            var newpass = temp + password;
-            var hashed_password = crypto.createHash('sha512').update(newpass).digest("hex");
-            
-            if(app_code=="mzf")
+            console.log("Email id found.");
+            if(users.length != 0)
             {
-                if(hash_db == hashed_password)
-                {
-                    callback({'response':"Login Sucess",'token':unique_id,'success':true,'_id':id});
+                var unique_id = users[0].person_credentials.token;
+                var temp = users[0].person_credentials.salt;
+                var hash_db = users[0].person_credentials.hashed_password;
+                var name = users[0].person_credentials.name;
+                var id = users[0]._id;
+                var newpass = temp + password;
+                var hashed_password = crypto.createHash('sha512').update(newpass).digest("hex");
+
+                    if(hash_db == hashed_password)
+                    {
+                        callback({'response':"Login Sucess",'token':unique_id,'success':true,'_id':id});
+                    }
+                    else
+                    {
+                        callback({'response':"Invalid Password",'success':false});
+                    }
                 }
-                else
-                {
-                    callback({'response':"Invalid Password",'success':false});
-                }
+
             }
             else 
             {
-                callback({'response':"Your app is outdated",'success':false});
+                callback({'response':"Email id not valid.",'success':false});
             }
-        }
-        else 
-        {
-            callback({'response':"Email id not valid.",'success':false});
-        }
-    });
+        });
+    }
+    else 
+    {
+        callback({'response':"Your app is outdated",'success':false});
+    }
 }
--- config/register.js
+++ config/register.js
@@ -8,40 +8,40 @@ var user = require('../config/models');
 exports.register = function(app_code,email,password,name,phone,gender,age,grade_10,sleep_hours,test_rank,institute,callback)
 {
     var x = email;
-    if(!(x.indexOf("@")==x.length))
-    {        
-        if (password.length > 4)
-        {
-            var temp =rand(160, 36);
-            var newpass = temp + password;
-            var token = crypto.createHash('sha512').update(email +rand).digest("hex");
-            var hashed_password = crypto.createHash('sha512').update(newpass).digest("hex");
+    if(app_code == "mzf") 
+    {
+        if(!(x.indexOf("@")==x.length))
+        {        
+            if (password.length > 4)
+            {
+                var temp =rand(160, 36);
+                var newpass = temp + password;
+                var token = crypto.createHash('sha512').update(email +rand).digest("hex");
+                var hashed_password = crypto.createHash('sha512').update(newpass).digest("hex");
 
-            var newuser = new user(  
-                {
-                    "person_credentials" :
+                var newuser = new user(  
                     {
-                        "institute" : institute,
-                        "test_rank" : test_rank,
-                        "gender" : gender,
-                        "age" : age,
-                        "grade_10" : grade_10,
-                        "sleep_hours" : sleep_hours,
-                        "temp_str" : "",
-                        "salt"  : temp,
-                        "token" : token,
-                        "name"  : name,
-                        "email" : email,
-                        "phone" : phone,
-                        "hashed_password" : hashed_password
+                        "person_credentials" :
+                        {
+                            "institute" : institute,
+                            "test_rank" : test_rank,
+                            "gender" : gender,
+                            "age" : age,
+                            "grade_10" : grade_10,
+                            "sleep_hours" : sleep_hours,
+                            "temp_str" : "",
+                            "salt"  : temp,
+                            "token" : token,
+                            "name"  : name,
+                            "email" : email,
+                            "phone" : phone,
+                            "hashed_password" : hashed_password
+                        }
                     }
-                }
-            );
+                );
 
-            user.find({'person_credentials.email': email},function(err,users){
-                var len = users.length;
-                if(app_code == "mzf")
-                {
+                user.find({'person_credentials.email': email},function(err,users){
+                    var len = users.length;
                     if(len == 0)
                     {
                         newuser.save(function (err){
@@ -52,20 +52,20 @@ exports.register = function(app_code,email,password,name,phone,gender,age,grade_
                     {
                         callback({'response':"Email already Registered",success:false});
                     }
-                }
-                else
-                {
-                    callback({'response':"Your app is outdated...",success:false});
-                }
-            });
+                });
+            }
+            else 
+            {
+                callback({'response':"Password Weak",success:false});
+            }
         }
-        else 
+        else
         {
-            callback({'response':"Password Weak",success:false});
+            callback({'response':"Email Not Valid",success:false});
         }
     }
     else
     {
-        callback({'response':"Email Not Valid",success:false});
+        callback({'response':"Your app is outdated",success:false});   
     }
 }
